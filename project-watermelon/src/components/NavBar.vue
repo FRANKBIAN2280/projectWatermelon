2366f5c73f3a9f3ecc9b84ed59efe4db5995eccb
Zhongxuan Song <zhs61@pitt.edu>
2024-10-22 18:22:20 -0400

    add the template and the components of nav bar

---
+++ project-watermelon/src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<script setup></script>
+<template>
+  <div class="nav-bar">
+    <div class="nav">
+      <div class="logo">Paper trade</div>
+      <ul>
+        <li>Home</li>
+        <li>Stands</li>
+        <li>Market</li>
+      </ul>
+    </div>
+    <div class="nav" style="gap: 10px">
+      <div class="nav-button">Sign up</div>
+      <div class="nav-button">Log in</div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.nav-bar {
+  display: flex;
+  align-content: center;
+  justify-content: space-between;
+  background-color: black;
+  padding: 0px 16px;
+}
+.nav {
+  display: flex;
+  align-items: center;
+}
+.logo {
+  margin: auto;
+  color: white;
+  font-size: 24px;
+}
+ul {
+  display: flex;
+  list-style-type: none;
+  justify-content: space-between;
+  gap: 30px;
+  color: white;
+}
+.nav-button {
+  color: white;
+}
+</style>
